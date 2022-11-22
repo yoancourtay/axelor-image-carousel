6d54ff6cfa0c28a09c0dea61c752874939ca0cb9
yoancourtay <115277511+yoancourtay@users.noreply.github.com>
2022-11-22 16:27:44 +0100

    Getting to MetaFile

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -5,17 +5,42 @@ export default class Carousel extends Component {
         super(props)
       
         this.state = {
-            pictures: []
+            pictures: [],
+            axelorAppURL: 'http://localhost:8080/pizza-planet',
+            mainDomain: 'com.axelor.photoalbum.db.Album',
+            subDomain: 'com.axelor.photoalbum.db.Photo',
+            subDomFieldName : 'photoList',
+            paramName: 'albumId'
         }
+        
         this.getPictures = this.getPictures.bind(this)
     }
 
     getPictures () {
         const params = new URLSearchParams(document.location.search)
-        const id = params.get('albumId')
+        const id = params.get(this.state.paramName)
+        console.log('Given ID: ' + id);
 
         if (id) {
-            
+            fetch(`${this.state.axelorAppURL}/ws/rest/${this.state.mainDomain}/${id}/`, {method: 'GET'})
+            .then((response) => response.json())
+            .then((json) => {
+                // console.log(json.data[0][this.state.subDomFieldName])
+                json.data[0][this.state.subDomFieldName].forEach(subDomain => {
+                    fetch(`${this.state.axelorAppURL}/ws/rest/${this.state.subDomain}/${subDomain.id}/`, {method: 'GET'})
+                    .then((response) => response.json())
+                    .then((json) => {
+                        fetch(`${this.state.axelorAppURL}/ws/rest/com.axelor.meta.db.MetaFile/${json.data[0].picture.id}/`, {method: 'GET'})
+                        .then((response) => response.json())
+                        .then((json) => {
+                            console.log(json.data[0])
+                        })
+                        .catch(err => console.error(err))
+                    })
+                    .catch(err => console.error(err))
+                });
+            })
+            .catch(err => console.error(err))
         }
     }
 
