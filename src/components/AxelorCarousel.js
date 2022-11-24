0b8a1d9b71a917ea7d3200fd75c81a35f10f71c4
yoancourtay <115277511+yoancourtay@users.noreply.github.com>
2022-11-24 16:11:47 +0100

    Update AxelorCarousel.js

--- src/components/AxelorCarousel.js
+++ src/components/AxelorCarousel.js
@@ -7,14 +7,14 @@ export default class AxelorCarousel extends Component {
       
         this.state = {
             pictures: [],
-            axelorAppName: 'pizza-planet',
-            paramName: 'albumId',
+            id: null,
             
             mainDomain: 'com.axelor.photoalbum.db.Album',
             subDomain: 'com.axelor.photoalbum.db.Photo',
             subDomFieldName : 'photoList',
             
-            
+            axelorAppName: 'pizza-planet',
+            paramName: 'albumId',
             domToSubDomField: [
                 {
                     domain: 'com.axelor.photoalbum.db.Album',
@@ -32,34 +32,60 @@ export default class AxelorCarousel extends Component {
 
     getPictures () {
         const params = new URLSearchParams(document.location.search)
-        const id = params.get(this.state.paramName)
-        console.log(this.state.domToSubDomField.length)
-        
-        let cId = params.get(this.state.paramName)
-        this.state.domToSubDomField.forEach(group => {
-            
-        });
+        this.setState({id: params.get(this.state.paramName)}, async () => {
+            if (this.state.id) {
+                // this.state.domToSubDomField.forEach(group => {
+                //     //Go through domains
+                //     fetch(`/${this.state.axelorAppName}/ws/rest/${group.domain}/${this.state.id}/`, {method: 'GET'})
+                //     .then(response => response.json())
+                //     .then(json => {
+                //         if (group.field) {
+                //             this.setState({id: json.data[0][group.field].id})
+                //             console.log("Still more fields")
+                //         } else {
+                //             console.log("No more fields")
+                //         }                        
+                //     })
+                //     .catch(err => console.error(err))
+                // })
 
-        if (id) {
-            fetch(`/${this.state.axelorAppName}/ws/rest/${this.state.mainDomain}/${id}/`, {method: 'GET'})
-            .then(response => response.json())
-            .then(json => {
-                json.data[0][this.state.subDomFieldName].forEach(subDomain => {                                       
-                    fetch(`/${this.state.axelorAppName}/ws/rest/${this.state.subDomain}/${subDomain.id}/`, {method: 'GET'})
+                for (const group of this.state.domToSubDomField) {
+                    //Go through domains
+                    //TODO check if field is array and go through it
+                    await fetch(`/${this.state.axelorAppName}/ws/rest/${group.domain}/${this.state.id}/`, {method: 'GET'})
                     .then(response => response.json())
-                    .then(json => this.setState(prevState => {
-                        return {
-                            pictures: [
-                            ...prevState.pictures, 
-                            `/${this.state.axelorAppName}/ws/rest/com.axelor.meta.db.MetaFile/${json.data[0].picture.id}/content/download`
-                        ]
-                        }
-                    }))
+                    .then(json => {
+                        if (group.field) {
+                            this.setState({id: json.data[0][group.field].id})
+                            console.log("Still more fields")
+                        } else {
+                            console.log("No more fields")
+                        }                        
+                    })
                     .catch(err => console.error(err))
-                });
-            })
-            .catch(err => console.error(err))
-        }
+                }
+                
+                
+                // fetch(`/${this.state.axelorAppName}/ws/rest/${this.state.mainDomain}/${id}/`, {method: 'GET'})
+                // .then(response => response.json())
+                // .then(json => {
+                //     json.data[0][this.state.subDomFieldName].forEach(subDomain => {                                       
+                //         fetch(`/${this.state.axelorAppName}/ws/rest/${this.state.subDomain}/${subDomain.id}/`, {method: 'GET'})
+                //         .then(response => response.json())
+                //         .then(json => this.setState(prevState => {
+                //             return {
+                //                 pictures: [
+                //                 ...prevState.pictures, 
+                //                 `/${this.state.axelorAppName}/ws/rest/com.axelor.meta.db.MetaFile/${json.data[0].picture.id}/content/download`
+                //             ]
+                //             }
+                //         }))
+                //         .catch(err => console.error(err))
+                //     });
+                // })
+                // .catch(err => console.error(err))
+            }
+        })
     }
 
     componentDidMount() { 
