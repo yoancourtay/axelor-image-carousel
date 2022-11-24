1cb5b2babcc224d5967ef22f9e761d8da518ecf0
yoancourtay <115277511+yoancourtay@users.noreply.github.com>
2022-11-24 09:45:42 +0100

    New bug found, but not fixed

--- src/components/AxelorCarousel.js
+++ src/components/AxelorCarousel.js
@@ -20,30 +20,20 @@ export default class AxelorCarousel extends Component {
     getPictures () {
         const params = new URLSearchParams(document.location.search)
         const id = params.get(this.state.paramName)
-        // console.log('Given ID: ' + id)
 
         if (id) {
             fetch(`/${this.state.axelorAppName}/ws/rest/${this.state.mainDomain}/${id}/`, {method: 'GET'})
-            .then((response) => response.json())
-            .then((json) => {
-                // console.log(json.data[0][this.state.subDomFieldName])
+            .then(response => response.json())
+            .then(json => {
                 json.data[0][this.state.subDomFieldName].forEach(subDomain => {                                       
                     fetch(`/${this.state.axelorAppName}/ws/rest/${this.state.subDomain}/${subDomain.id}/`, {method: 'GET'})
-                    .then((response) => response.json())
-                    .then((json) => {
-                        
-                        let pictures = [...this.state.pictures]
-                        pictures.push(`/${this.state.axelorAppName}/ws/rest/com.axelor.meta.db.MetaFile/${json.data[0].picture.id}/content/download`)
-                        this.setState({ pictures })
-                            
-                        
-                        // fetch(`/${this.state.axelorAppName}/ws/rest/com.axelor.meta.db.MetaFile/${json.data[0].picture.id}/content/download`, {method: 'GET'})
-                        // .then((response) => console.log(response.url))
-                        // // .then((json) => {
-                        // //     console.log(json.data[0])
-                        // // })
-                        // .catch(err => console.error(err))
-                    })
+                    .then(response => response.json())
+                    .then(json => this.setState({
+                        pictures: [// Too fast for React ?
+                            ...this.state.pictures, 
+                            `/${this.state.axelorAppName}/ws/rest/com.axelor.meta.db.MetaFile/${json.data[0].picture.id}/content/download`
+                        ]
+                    }))
                     .catch(err => console.error(err))
                 });
             })
